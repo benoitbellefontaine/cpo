f76fdd76553ab93c5f2e31305f4fbb9b703cf584
benoitbellefontaine <benoit.bellefontaine@gmail.com>
2018-09-26 21:34:17 -0400

    Start with create-react-app

--- src/index.js
+++ src/index.js
@@ -41,8 +41,3 @@ ReactDOM.render(
 registerServiceWorker();
 
 export default store;
-//export { store };
-export { select, selectAll } from 'd3-selection';  
-//export { json } from 'd3-request';  
-//export { geoAlbersUsa, geoPath, geoGraticule } from 'd3-geo';  
-export { queue } from 'd3-queue';
